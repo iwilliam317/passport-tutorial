b1d2885f2f22e0f9749d942e0574fee7db5e85e3
william <willshinji@gmail.com>
2018-09-29 15:10:18 -0300

    set error handling

--- app.js
+++ app.js
@@ -44,3 +44,14 @@ if(!isProduction) {
     });
   });
 }
+
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
+});
