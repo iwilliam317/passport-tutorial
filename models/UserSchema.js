2f19e97a7dc6b6fe631dab6a115e36956828cd4b
william <willshinji@gmail.com>
2018-09-30 00:44:36 -0300

    validatePassword done

--- models/UserSchema.js
+++ models/UserSchema.js
@@ -12,3 +12,8 @@ UsersSchema.methods.setPassword = function(password) {
   this.salt = crypto.randomBytes(16).toString('hex');
   this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
 };
+
+UsersSchema.methods.validatePassword = function(password) {
+  const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+  return this.hash === hash;
+};
