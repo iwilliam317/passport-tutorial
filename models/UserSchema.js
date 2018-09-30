4e42d23a89a4af4355a750f47033b70f6d292e7f
william <willshinji@gmail.com>
2018-09-30 00:20:10 -0300

    define UserSchema

--- models/UserSchema.js
+++ models/UserSchema.js
@@ -1,2 +1,9 @@
 const mongoose = require('mongoose');
 const crypto = require('crypto');;
+const jwt = require('jsonwebtoken');
+
+const UserSchema = new mongoose.Schema({
+  email: String,
+  hash: String,
+  salt: String,
+})
