ea7c8ef16410dfca28e388a8cc2856923cd070b2
william <willshinji@gmail.com>
2018-10-01 13:31:53 -0300

    generate token im User model

--- models/UserSchema.js
+++ models/UserSchema.js
@@ -17,3 +17,16 @@ UsersSchema.methods.validatePassword = function(password) {
   const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
   return this.hash === hash;
 };
+
+UsersSchema.methods.generateJWT = function() {
+  const today = new Date();
+  const expirationDate = new Date(today);
+  expirationDate.setDate(today.getDate() + 60);
+
+  return jwt.sign({
+    email: this.email,
+    id: this._id,
+    exp: parseInt(expirationDate.getTime() / 1000, 10),
+  }, 'secret');
+}
+
